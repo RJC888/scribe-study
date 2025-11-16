c764b67f4bbbc89fa9a267b186f66cf793c818b5
RJC888 <121136782+RJC888@users.noreply.github.com>
2025-11-16 00:18:02 -0500

    Update Playwright.config.js

--- Playwright.config.js
+++ Playwright.config.js
@@ -1,9 +1,6 @@
-// playwright.config.js
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 30000,
-
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     headless: true,

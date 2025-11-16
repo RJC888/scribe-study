7b67d80a8780b65265a0d67a30e2c5595491b21e
RJC888 <121136782+RJC888@users.noreply.github.com>
2025-11-16 06:55:01 -0500

    Update Playwright.config.js

--- Playwright.config.js
+++ Playwright.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   },
 
   webServer: {
-    command: 'npx serve frontend -l 3000',
+    command: 'npx serve -l 3000 frontend',
     port: 3000,
-    timeout: 20000,
-    reuseExistingServer: !process.env.CI,
-  },
+    timeout: 30000,
+    reuseExistingServer: !process.env.CI
+  }
 });
